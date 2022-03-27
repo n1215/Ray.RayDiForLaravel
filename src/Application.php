ba64555fb42c0016e7a2ca003d8eec2f3c6d2ef1
n1215 <knakae1215@gmail.com>
2022-03-27 10:29:43 +0900

    prioritize the illuminate container.

--- src/Application.php
+++ src/Application.php
@@ -2,6 +2,7 @@
 
 namespace Ray\RayDiForLaravel;
 
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Ray\Di\Exception\Unbound;
 use Ray\Di\InjectorInterface;
 
@@ -18,9 +19,13 @@ class Application extends \Illuminate\Foundation\Application
     protected function resolve($abstract, $parameters = [], $raiseEvents = true)
     {
         try {
-            return $this->injector->getInstance($abstract);
-        } catch (Unbound $e) {
             return parent::resolve($abstract, $parameters, $raiseEvents);
+        } catch (BindingResolutionException $e) {
+            try {
+                return $this->injector->getInstance($abstract);
+            } catch (Unbound $e) {
+                throw new BindingResolutionException("failed to resolve {$abstract}.", $e->getCode(), $e);
+            }
         }
     }
 }
